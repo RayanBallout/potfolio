8053bea181570d82ea0915dfef6cdb02f7684c62
RayanBallout <rayanballout14@gmail.com>
2022-03-13 18:55:45 +0200

    Adding the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,29 @@
 import React from "react"
+import CTA from "./CTA"
 import "./header.css"
+import ME from "../../assets/me.png"
+import HeaderSocials from "./HeaderSocials"
 
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<header>
+			<div className="container header__container">
+				<h5>Hello I'm</h5>
+				<h1>Rayan Ballout</h1>
+				<h5 className="text-light">Junior Fullstack Developer</h5>
+				<CTA />
+				<HeaderSocials />
+
+				<div className="me">
+					<img src={ME} alt="me" />
+				</div>
+
+				<a href="#contact" className="scroll__down">
+					Scroll Down
+				</a>
+			</div>
+		</header>
+	)
 }
 
 export default Header
